2f4f9ccf6de91af219d802ffaf28cd7454b880b4
Viktor Kálmán <vkalman@consumeraffairs.com>
2024-11-05 16:01:40 +0100

    use publicPath as base in router

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import EntityList from './components/EntityList.vue';
 
 const router = createRouter({
+  base: process.env.BASE_URL,
   history: createWebHistory(),
   routes: [
     { path: '/:ids*', name: "list", component: EntityList},
