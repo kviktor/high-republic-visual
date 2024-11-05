98b4e24b0615072c152a0ccce980d47e2b44e944
Viktor Kálmán <vkalman@consumeraffairs.com>
2024-11-05 16:05:13 +0100

    hardcode base for router

--- src/router.js
+++ src/router.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import EntityList from './components/EntityList.vue';
 
 const router = createRouter({
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
+  base: "/high-republic-visual/",
   history: createWebHistory(),
   routes: [
     { path: '/:ids*', name: "list", component: EntityList},
